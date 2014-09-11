33d3fda89b3557a5c01494a37864ee9663a77718
Nishit Parekh <ug201211022@iitj.ac.in>
2014-09-12 00:21:21 +0530

    Schedule: Update 2

--- scheduler.py
+++ scheduler.py
@@ -25,27 +25,79 @@ class Scheduler():
 		self.sideLineSchedule = []
 		for i in range (1,11):
 			if i<6:
-				self.sideLineSchedule.append({"Number": i, "Location": "LEFT"})
+				self.sideLineSchedule.append({"Number": i, "Occupancy": "EMPTY", "Code": "0", "Location": "LEFT"})
 			else:
-				self.sideLineSchedule.append({"Number": i, "Location": "RIGHT"})
+				self.sideLineSchedule.append({"Number": i, "Occupancy": "EMPTY", "Code": "0", "Location": "RIGHT"})
 
 
 		for train in self.trainSchedule:
 			if train["Type"]=="Passing":
-				train["PlatformTime"] = 15
-			else:
 				train["PlatformTime"] = 5
+			else:
+				train["PlatformTime"] = 15
 
 		for train in self.trainSchedule:
 			print self.timeToInt(train["Scheduled_Arrival_Time"])
+
+
 
 	def makeSchedule(self):
 
 		for self.timer in range(0,60*24 + 1):
 
-			# Platform Trains
-			# Waiting Trains
-			# Incoming Trains
+			self.schedulingPlatformTrains()
+			self.schedulingWaitingTrains()
+			self.schedulingIncomingTrains()
+
+
+
+	def schedulingPlatformTrains(self):
+		for train in self.trainSchedule:
+
+			if train["Status"]=="ON_PLATFORM":
+				currentTrain = self.getTrain(train["Code"])
+
+				if train["PlatformTime"]==0:
+					currentPlatform = self.getPlatform(train["PlatformNumber"])
+
+					currentTrain["Status"] = "DEPARTED"
+					currentTrain["PlatformNumber"] = 0
+
+					currentPlatform["Code"] = 0
+					currentPlatform["Occupancy"] = "EMPTY"
+				else:
+					currentTrain["PlatformTime"] = currentTrain["PlatformTime"] - 1
+
+
+	def schedulingWaitingTrains(self):
+		for train in self.trainSchedule:
+
+			if train["Status"]=="WAITING":
+				currentTrain = self.getTrain(train["Code"])
+
+
+
+	def schedulingIncomingTrains(self):
+		i=1
+
+
+
+
+
+	def getTrain(self,code):
+		for train in self.trainSchedule:
+			if train["Code"]==code:
+				return train
+
+	def getPlatform(self,number):
+		for platform in self.platformSchedule:
+			if platform["Number"]==number:
+				return platform
+
+	def getSideLine(self,number):
+		for sideLine in self.sideLineSchedule:
+			if sideLine["Number"]==number:
+				return sideLine
 
 	def timeToInt(self,timeString):
 		timeList = timeString.split(':')
@@ -54,6 +106,6 @@ class Scheduler():
 	def intToTime(self,timeInt):
 		return(str(int(timeInt/60)) + ":" + str(timeInt%60))
 
-Scheduler()
+Scheduler().makeSchedule()
 
 		
