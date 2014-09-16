600f2eeb3a885861d7267f31e3b1d2a11dd36831
Nishit Parekh <ug201211022@iitj.ac.in>
2014-09-16 10:50:20 +0530

    Remove garbage

--- prefill.py
+++ prefill.py
@@ -8,8 +8,8 @@ deleteTrain("13763")
 
 addTrain("Mandore Exp.","12483","19:30","West","NOT_ARRIVED","Originating")
 addTrain("Suryanagari Exp.","12481","17:30","East","NOT_ARRIVED","Destination")
-addTrain("Howrah Exp.","12501","05:20","West","WAITING","Passing")
-addTrain("Rajdhani Exp.","13763","12:00","East","DEPARTED","Originating")
+addTrain("Howrah Exp.","12501","05:20","West","NOT_ARRIVED","Passing")
+addTrain("Rajdhani Exp.","13763","12:00","East","NOT_ARRIVED","Originating")
 
 
 #updateTrainArrivalTime("12483","21:45")
